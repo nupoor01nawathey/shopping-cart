782846c329a7ddbaf4518aab7f4bf8b4ab552424
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-11-05 20:59:48 +0530

    refactor app
    refactor app

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ const app = express();
 const adminRoutes = require('./routes/admin'),
       shopRoutes  = require('./routes/shop');
 
+const errorController = require('./controllers/error');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin',adminRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.notFound);
 
 app.listen(3000);
