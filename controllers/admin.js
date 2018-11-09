c5678cab55ceba3b7673ba34c1782bd38e785dd3
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-11-09 21:17:33 +0530

    use mysql to get prod and prod details
    use mysql to get prod and prod details

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,13 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const title       = req.body.title,
+        imageUrl    = req.body.imageUrl,
+        price       = req.body.price,
+        description = req.body.description;
+        
+  const product     = new Product(title, imageUrl, description, price);
+  product.save()
+  .then((product) => {
+    console.log(product);
+    res.redirect('/')
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 exports.getProducts = (req, res, next) => {
