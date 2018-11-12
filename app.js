db70a89e7f1a390ff40862b16b30e8f6983983e3
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-11-12 16:48:01 +0530

    update order routes and add session
    update order routes and add session

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
-const path = require('path');
+const path            = require('path'),
+      express         = require('express'),
+      bodyParser      = require('body-parser'),
+      session         = require('express-session'),
+      MySQLStore      = require('express-mysql-session')(session),
+      app             = express();
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const sequelize       = require('./util/database'),
+      Product         = require('./models/product'),
+      User            = require('./models/user'),
+      Cart            = require('./models/cart'),
+      CartItem        = require('./models/cart-item'),
+      Order           = require('./models/order'),
+      OrderItem       = require('./models/order-item');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
 
-
-const app = express();
+var options = {
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'YOUR_MYSQL_PASSWORD',
+	database: 'ShoppingCart',
+};
+const sessionStore    = new MySQLStore(options);
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const shopRoutes  = require('./routes/shop');
+const authRoutes  = require('./routes/auth');
 
 // process.on('unhandledRejection', up => { throw up }) to match unhandledPromiseRejection Warnings
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(session({ 
+    secret: 'CatSAreTHECUte$T', 
+    resave: false, 
+    saveUninitialized: false,
+    store: sessionStore
+}));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
+    User.findById(1)
     .then(user => {
         req.user = user;
+        // console.log('Magic methods in scope', Object.keys(req.user.__proto__));
         next();
     })
     .catch(err => {
@@ -36,28 +53,40 @@ app.use((req, res, next) => {
     });
 });
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
 app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
-// .sync({force: true})
-.sync()
-.then(result => {
-    //console.log(result);
+// .sync({force: true}) // for modifying existing table not to use in prod
+  .sync()
+  .then(result => {
     return User.findByPk(1);
-    
-}).then(user => {
+  }).then(user => {
     if(!user) {
         return User.create({ name: 'Nupoor', email: 'test@test.com' });
     }
     return user;
 })
 .then(user => {
-    console.log(user);
+    return user.createCart();
+})
+.then(cart => {
     app.listen(3000);
 })
 .catch(err => {
     console.log(err);
 });
+
