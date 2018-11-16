7fe6a6737b8b57f03f99ade8ec6bb98a227d5617
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-11-16 22:12:54 +0530

    nodemailer
    add sendmail for sending email notifications after successful signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,48 @@
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
+const bcrypt            = require('bcryptjs'),
+      nodemailer        = require('nodemailer'),
+      sendgridTransport = require('nodemailer-sendgrid-transport');
+
+const User              = require('../models/user');
+
+const transporter       = nodemailer.createTransport(sendgridTransport({
+    auth : {
+        api_key : 'YOUR_AUTH_KEY'
+    }
+}));
 
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
-        isAuthenticated: req.session.isLoggedIn
+        errorMessage: req.flash('error')
+        //isAuthenticated: req.session.isLoggedIn
     });
 }
 
-exports.postLogin = (req, res, next) => {
+exports.postLogin  = (req, res, next) => {
     const email    = req.body.email,
           password = req.body.password;
     User.findOne({ where: {email: email} })
     .then(user => { 
         if(!user) {
+            req.flash('error', 'Incorrect email id');
             return res.redirect('/login');
         }
         bcrypt
             .compare(password, user.password)
             .then(doMatch => {
-                if(doMatch) {
+                if(doMatch) { 
                     req.session.isLoggedIn = true;
                     req.session.user = user;
                     user.createCart();
                     return req.session.save((err) => { // async alert: save session first and then redirect
-                        console.log(err);
+                        // console.log(err);   
                         res.redirect('/');
                     });
+                } else {
+                    req.flash('error', 'Incorrect password');
+                    res.redirect('/login');
                 }
-                res.redirect('/login');
             })     
             .catch(err => console.log(err));
     })
@@ -40,7 +53,7 @@ exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         pageTitle: 'Signup',
         path: '/signup',
-        isAuthenticated: req.session.isLoggedIn
+        errorMessage: req.flash('error')
     });
 }
 
@@ -51,7 +64,7 @@ exports.postSignup = (req, res, next) => {
     User.findOne({ where: {email: email} })
     .then(foundEmail => {
         if(foundEmail) {
-            console.log('user already exists, please try with new email id');
+            req.flash('error','user already exists, please try with new email id');
             return res.redirect('/signup');
         } 
         return bcrypt.hash(password, 12)
@@ -62,13 +75,19 @@ exports.postSignup = (req, res, next) => {
             })
             .save()
             .then(result => {
-                console.log('user created successfully');
+                res.redirect('/login');   
                 result.createCart();
-                res.redirect('/login');    
+                return transporter.sendMail({
+                        to: email,
+                        from: 'node-shop@donotreply.com',
+                        subject: 'Signup successful!',
+                        html: '<h3>Welcome to the node-shop account. You have successfully signed up!</h3>'
+                    })
+                })
+                .catch(err => console.log(err)); 
             })
-            .catch(err => console.log(err));    
+            .catch(err => console.log(err));
         })
-    })
     .catch(err => console.log(err));
 }
 
